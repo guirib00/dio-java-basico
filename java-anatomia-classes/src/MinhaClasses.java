1ba03a55ee2b9e6939af904a7329f126cc78c602
Guilherme Ribeiro Souza <guilhermerib2013@hotmail.com>
2024-05-22 00:59:57 -0300

    Atualização anatomia classes - 1 e 2

--- java-anatomia-classes/src/MinhaClasses.java
+++ java-anatomia-classes/src/MinhaClasses.java
@@ -1,8 +1,31 @@
 public class MinhaClasses {
 
     public static void main(String[] args) {
-        System.out.println("Ola");
+        String primeiroNome = "Guilherme";
+        String segundoNome = "Ribeiro";
 
+        int anoFabricacao = 2023;
+
+        boolean verdadeira = false;
+
+        int numero1 = 10;
+        int numero2 = 20;
+
+        int somaNumeros = somar(numero1, numero2);
+
+        System.out.println(somaNumeros);
+        System.out.println(nomeCompleto(primeiroNome, segundoNome));
+
+    }
+
+    public static int somar(int n1, int n2){
+        int soma = n1+n2;
+        
+        return soma;
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
